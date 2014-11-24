bdf94ff396118a300a1ebcfe29afc30f42d59511
Loïc Gerard <lgerard@diatem.net>
2014-11-24 08:48:45 +0100

    Amelioration de la gestion des erreurs. Possibilite de repasser en gestion native PHP des erreurs a des fins de debug

--- config/errors_default.php
+++ config/errors_default.php
@@ -1,3 +1,4 @@
 <?php
 
-define('ERROR_REPORT', false);
+define('ERROR_REPORT', false);   //Report des erreur en visuel. (false pour un serveur en production)
+define('ERROR_NATIVE', false);  //Usage de la gestion des erreurs native de PHP (false pour un serveur en production)
--- run/igniter.php
+++ run/igniter.php
@@ -9,14 +9,15 @@ if(ERROR_REPORT){
     ini_set('display_errors', 'Off');
 }
 
-
-register_shutdown_function(function() {
-    $error = error_get_last();
-    if($error !== null){
-        echo '<h1>Erreur Fatale !</h1><b>erreur grave à l\'initialisation du serveur. La configuration est certainement erronnée.</b><br><hr>';
-        var_dump($error);
-    }
-});
+if(!ERROR_NATIVE){
+    register_shutdown_function(function() {
+        $error = error_get_last();
+        if($error !== null){
+            echo '<h1>Erreur Fatale !</h1><b>erreur grave à l\'initialisation du serveur. La configuration est certainement erronnée.</b><br><hr>';
+            var_dump($error);
+        }
+    });
+}
 
 //Initialize launcher for Aspic classes
 include '../api/aspiccore.php';
@@ -29,10 +30,11 @@ include_once Config::getJinLibraryLocation().'launcher.php';
 //Initialize shortcuts
 include_once 'shortcuts.php';
 
-set_error_handler(array( 'aspic\publicappz\PublicAppz', 'errorHandler_standard'));
-set_exception_handler(array('aspic\publicappz\PublicAppz', 'errorHandler_exceptions'));
-register_shutdown_function(array('aspic\publicappz\PublicAppz', 'errorHandler_fatal'));
-
+if(!ERROR_NATIVE){
+    set_error_handler(array( 'aspic\publicappz\PublicAppz', 'errorHandler_standard'));
+    set_exception_handler(array('aspic\publicappz\PublicAppz', 'errorHandler_exceptions'));
+    register_shutdown_function(array('aspic\publicappz\PublicAppz', 'errorHandler_fatal'));
+}
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sid'])){
     //Check jeton
