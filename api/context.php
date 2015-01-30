62b1369816caaed6b587b1d580779d55aa15ed6c
Loïc Gerard <lgerard@diatem.net>
2015-01-30 12:27:14 +0100

    Fix. Crash contexte si la cle nexiste pas

--- api/context.php
+++ api/context.php
@@ -6,30 +6,33 @@ namespace aspic;
  * Classe permettant de gérer les données de contexte
  * @author Loïc Gerard <lgerard@diatem.net>
  */
-class Context{
-	/**
-	 * Données stockées
-	 * @var array
-	 */
+class Context {
+
+    /**
+     * Données stockées
+     * @var array
+     */
     private static $data = array();
-    
 
     /**
      * Enregistre une donnée
      * @param  string $key  Clé
      * @param  mixed $data Donnée
      */
-    public static function put($key, $data){
-		self::$data[$key] = $data;
+    public static function put($key, $data) {
+	self::$data[$key] = $data;
     }
-    
 
     /**
      * Retourne la valeur d'une clé enregistrée
      * @param  string $key Clé
      * @return mixed
      */
-    public static function get($key){
-		return self::$data[$key];
+    public static function get($key) {
+	if(!isset(self::$data[$key])){
+	    return null;
+	}
+	return self::$data[$key];
     }
+
 }
