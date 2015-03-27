858f3835552caecad3c0f03759cb22346d262012
Loïc Gerard <lgerard@diatem.net>
2015-03-27 08:26:28 +0100

    Preparation de la version

--- api/config.php
+++ api/config.php
@@ -20,7 +20,7 @@ class Config {
      * Version du serveur
      * @var string
      */
-    private static $version = '1.2.0';
+    private static $version = '1.2.2';
 
     /**
      * Retourne le nom du template de rendu utilisé
